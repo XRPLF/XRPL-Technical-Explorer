111598e98953af393d53856d8fbfaa73835ca8d3
Wietse Wind <mail@wietse.com>
2021-07-07 10:21:25 +0200

    Add $net (live, test, hooks) for context

--- src/plugins/xrpl.js
+++ src/plugins/xrpl.js
@@ -3,6 +3,17 @@ import { XrplClient } from 'xrpl-client'
 export default {
   async install (Vue, options) {
     Vue.prototype.$ws = new XrplClient(process?.env?.VUE_APP_WSS_ENDPOINT)
+
+    const endpoint = String(process?.env?.VUE_APP_WSS_ENDPOINT || '')
+
+    const net = {
+      live: endpoint === '' || endpoint.match(/xrplcluster|xrpl\.ws|xrpl\.link|s[12]\.ripple\.com/),
+      test: endpoint.match(/rippletest|\/testnet\.xrpl-labs/),
+      hooks: endpoint.match(/hooks/)
+    }
+
+    Vue.prototype.$net = net
+
     Vue.prototype.$ws.on('ledger', ledger => Vue.prototype.$events.emit('ledger', ledger))
     console.info('Connecting @ `plugins/xrpl`')
     await Vue.prototype.$ws.ready()
