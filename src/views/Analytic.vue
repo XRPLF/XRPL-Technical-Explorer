21307013fd99202ea61ed379c2519548bfd596f0
pakwai <virtue.sign@gmail.com>
2023-03-04 18:50:25 +0800

    Scaffold placeholder for analytic.

---
+++ src/views/Analytic.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="container-fluid pb-5">
+    <h4 class="nes blue">Analytic</h4>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Analytic',
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  },
+  async mounted() {
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
