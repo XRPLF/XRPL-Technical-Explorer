d0e80502f9da6466d04d9d180480147d5ce94e08
Lathan Britz <jucallme@gmail.com>
2023-11-07 23:18:26 -0300

    add b2m

--- src/views/Burn2Mint.vue
+++ src/views/Burn2Mint.vue
@@ -1,89 +1,85 @@
 <template>
     <div style="margin-left: 10px;">
-      <div style="display: flex; align-items: center; margin-bottom: 10px;">
-        <label style="margin: 0px;">Activated Ledger:</label>
-        <div style="margin-left: 10px;">{{ activatedLedger }}</div>
-      </div>
-      <form @submit.prevent="submitForm" style="margin-bottom: 10px;">
-        <div>
-          <label for="burnAmount">Burn Amount:</label>
-          <input
-            style="margin-left: 10px;"
-            type="number"
-            id="burnAmount"
-            v-model.number="burnAmount"
-            required
-          />
+        <div style="display: flex; align-items: center; margin-bottom: 10px;">
+            <label style="margin: 0px;">Activated Ledger:</label>
+            <div style="margin-left: 10px;">{{ activatedLedger }}</div>
         </div>
-        <button type="submit">Submit</button>
-      </form>
-      <div style="display: flex; align-items: center;">
-        <label style="margin: 0px;">Calculated Value:</label>
-        <div style="margin-left: 10px;">{{ calculatedValue }}</div>
-      </div>
+        <form @submit.prevent="submitForm" style="margin-bottom: 10px;">
+            <div>
+                <label for="burnAmount">Burn Amount:</label>
+                <input
+                    style="margin-left: 10px;"
+                    type="number"
+                    id="burnAmount"
+                    v-model.number="burnAmount"
+                    required
+                    />
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+        <div style="display: flex; align-items: center;">
+            <label style="margin: 0px;">Calculated Value:</label>
+            <div style="margin-left: 10px;">{{ calculatedValue }}</div>
+        </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
+</template>
+
+<script>
+export default {
     data () {
-      return {
-        ledgerIndex: null,
-        activatedLedger: null,
-        burnAmount: null,
-        calculatedValue: null
-      }
+        return {
+            ledgerIndex: null,
+            activatedLedger: null,
+            burnAmount: null,
+            calculatedValue: null
+        }
     },
     methods: {
-      addLedger (ledger) {
-        this.ledgerIndex = ledger.ledger_index
-        this.getActivationLedger()
-      },
-      async getActivationLedger () {
-        const data = await this.$store.getters.getClient.send({
-          command: 'ledger_entry',
-          index: '4BC50C9B0D8515D3EAAE1E74B29A95804346C491EE1A95BF25E4AAB854A6A651'
-        })
-        this.activatedLedger = data.node.XahauActivationLgrSeq || this.ledgerIndex
-      },
-      submitForm () {
-        function calculateHalving (value) {
-          if (value < 2000000) {
-            return 1
-          } else if (value >= 30000000) {
-            return 0
-          } else {
-            return 1 - (value - 2000000) / 28000000
-          }
+        addLedger (ledger) {
+            this.ledgerIndex = ledger.ledger_index
+            this.getActivationLedger()
+        },
+        async getActivationLedger () {
+            const data = await this.$store.getters.getClient.send({
+                command: 'ledger_entry',
+                index: '4BC50C9B0D8515D3EAAE1E74B29A95804346C491EE1A95BF25E4AAB854A6A651'
+            })
+            this.activatedLedger = data.node.XahauActivationLgrSeq || this.ledgerIndex
+        },
+        submitForm () {
+            function calculateHalving (value) {
+                if (value < 2000000) { return 1 }
+                if (value >= 30000000) { return 0 } 
+                return 1 - (value - 2000000) / 28000000
+            }
+            if (this.validateInput()) {
+                const ledgersClosed = this.ledgerIndex - this.activatedLedger
+                this.calculatedValue = calculateHalving(ledgersClosed) * this.burnAmount
+                return
+            }
+            console.log('Invalid input')
+        },
+        validateInput () {
+            return (
+                typeof this.burnAmount === 'number'
+            )
         }
-        if (this.validateInput()) {
-          const ledgersClosed = this.ledgerIndex - this.activatedLedger
-          this.calculatedValue = calculateHalving(ledgersClosed) * this.burnAmount
-        } else {
-          console.log('Invalid input')
-        }
-      },
-      validateInput () {
-        return (
-          typeof this.burnAmount === 'number'
-        )
-      }
     },
     mounted () {
-      if (typeof this.$store.getters.getClient !== 'undefined') {
-        if (this.$store.getters.getClient.getState().online) {
-          this.connected = true
+        if (typeof this.$store.getters.getClient !== 'undefined') {
+            if (this.$store.getters.getClient.getState().online) {
+                this.connected = true
+            }
         }
-      }
-  
-      this.$store.getters.getClient.on('ledger', this.addLedger)
-  
-      this.$store.getters.getClient.on('connected', () => {
-        this.connected = true
-      })
+
+        this.$store.getters.getClient.on('ledger', this.addLedger)
+
+        this.$store.getters.getClient.on('connected', () => {
+            this.connected = true
+        })
     },
     destroyed () {
         this.$store.getters.getClient.off('ledger', this.addLedger)
     }
-  }
-  </script>
+}
+</script>
