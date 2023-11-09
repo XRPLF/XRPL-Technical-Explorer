abbcbde8ba2553235e4c993032e7d448b4932df9
Lathan Britz <jucallme@gmail.com>
2023-11-09 12:06:50 -0300

    add ledger_entry_amendments command

--- src/plugins/commands.js
+++ src/plugins/commands.js
@@ -192,6 +192,16 @@ export const groupedCommands = [
         }
       },
       {
+        name: 'ledger_entry_amendments',
+        link: '/ledger_entry_amendments',
+        href: 'https://docs',
+        json: {
+          command: 'ledger_entry',
+          index: '7DB0788C020F02780A673DC74757F23823FA3014C1866E72CC4CD8B226CD6EF4',
+          ledger_index: 'validated'
+        }
+      },
+      {
         name: 'ledger_entry_amm',
         link: '/ledger_entry_amm',
         href: 'https://docs',
